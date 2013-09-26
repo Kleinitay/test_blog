f3e2a9c2c2fcc6689e97b7694ac838db80ff1360
Itay Klein <itay@crossrider.com>
2013-09-26 06:37:03 +0300

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+	def index
+  		@posts = Post.all
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def edit
+	  @post = Post.find(params[:id])
+	end
+
+	def update
+	  @post = Post.find(params[:id])
+	 
+	  if @post.update(params[:post])
+	    redirect_to @post
+	  else
+	    render 'edit'
+	  end
+	end
+
+	def create
+  		@post = Post.new(params[:post])
+ 
+  		if @post.save
+	    redirect_to @post
+	  else
+	    render 'new'
+	  end
+	end
+
+	def show
+  		@post = Post.find(params[:id])
+	end
+
+	def destroy
+	  @post = Post.find(params[:id])
+	  @post.destroy
+	 
+	  redirect_to posts_path
+	end
+ 
+private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '09952aaa7a652adbff108a842af781880e4e44b23172314aa539dfa8ce774575e83dc9337f32e93acf98b8c4fb3762a5e9dbc44f2b8a8e1d048317c0632c7657'
